f7927230b5fa4d294d013ee692c78b94c69d32cd
Arsen Anay <arsenanai@gmail.com>
2020-02-07 11:49:18 +0600

    logo added, calculator provided minor style fixes

--- index_files/script.js
+++ index_files/script.js
@@ -1,100 +1,5 @@
-
 $(function () {
     'use strict';
-    // navbarFixedTop
-    //$('.navbar .navbar-brand img').attr('height', '100px');
-    // --- script blocked from here ---
-    $('.navbar .navbar-brand img').attr('class', 'logo_long');
-    $('description').attr('style', 'display:none;');
-    $('phone').attr('class', 'phone');
-//    $(window).scroll(function () {
-//        if ($('.navbar').offset().top > 50) {
-//            $('.navbar-fixed-top').addClass('top-nav');
-//            $('.navbar .navbar-brand img').attr('src', 'img/logo/logo.svg');
-//            //$('.navbar .navbar-brand img').attr('height', '40px');
-//            $('.navbar .navbar-brand img').attr('class', 'logo_short');
-//            $('description').attr('style', 'display:box;float:left;color:#fff;');
-//            $('phone').attr('class', 'phone-scroll');
-//            
-//
-//        } else {           
-//            $('.navbar-fixed-top').removeClass('top-nav');
-//            $('.navbar .navbar-brand img').attr('src', 'img/logo/logo-string.svg');
-//            //$('.navbar .navbar-brand img').attr('height', '100px');
-//            $('.navbar .navbar-brand img').attr('class', 'logo_long');
-//            $('description').attr('style', 'display:none;');
-//            $('phone').attr('class', 'phone');
-//        }
-//    });
-    
-    // --- script blocked till here ---
-    
-    // navbarToggle
-    $('.navbar-toggle').on('click', function () {
-        $('.navbar-toggle').toggleClass('change');
-        $('.navbar').addClass('top-nav');
-    });
-    
-    // scrollSpy
-    /*$('body').scrollspy({target: ".navbar-nav", offset: 120});
-    $("a.nav-link").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 500, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
-    
-        // scrollSpy for topper links
-    $('body').scrollspy({target: ".navbar", offset: 0});
-    $("#btn-topper a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function () {
-                window.location.hash = hash;
-            });
-        }
-    });*/
-    
-    
-    // state
-    var a = 0;
-    $(window).scroll(function () {
-        var oTop = $('.state-items') - window.innerHeight;
-        if (a === 0 && $(window).scrollTop() > oTop) {
-            $('.stat-count').each(function () {
-                var $this = $(this),
-                    countTo = $this.attr('data-count');
-                $({
-                    countNum: $this.text()
-                }).animate({
-                    countNum: countTo
-                },
-                    {
-                        duration: 2000,
-                        easing: 'swing',
-                        step: function () {
-                            $this.text(Math.floor(this.countNum));
-                        },
-                        complete: function () {
-                            $this.text(this.countNum);
-                        alert('finished');
-                        }
-
-                    });
-            });
-            a = 1;
-        }
-    });
-    
-
     //scrollTop 
     var scrollButton = $(".scroll-top");
     $(window).scroll(function () {
@@ -104,6 +9,35 @@ $(function () {
             scrollButton.hide();
         }
     });
-    
 });
+/*
+ip
+5000
+17500
+26000
 
+too
+7500
+22500
+30000
+*/
+var app = new Vue({
+  el: '#pricecalc',
+  data: {
+    tip: 1,
+    service: 0,
+    tips:[
+        'ИП',
+        'ТОО',
+    ],
+    services:[
+        'Нулевая отчетность',
+        'Подготовка отчетности',
+        'Бухгалтерское сопровождение',
+    ],
+    prices:[
+        [5000,17500,26000],
+        [7500,22500,30000],
+    ]
+  }
+})
